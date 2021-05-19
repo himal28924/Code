bffde5177390ee165128805c253f379d4c1d8b49
himal28924 <himal28924@gmail.com>
2021-05-19 15:13:05 +0200

    '

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 // orange fish move to position while clicking
 $("html").on("click", function (e) {
   $("#fish1Id").stop();
@@ -7,23 +8,11 @@ $("html").on("click", function (e) {
   $("#fish1Id").myfunction();
 });
 
-// -----------------------
 
-// (function ($) {
-//   $.fn.loop = function () {
-//     i = 1;
-//     do {
-//       $("#fish2Id").myfunction();
-//       $("#fish1Id").myfunction();
-//       i++;
-//     } while (i < 200);
-//   };
-// });
-// (jQuery);
 
 // increasing the size of orange fish while clicking
 $("#fish1Id").on("dblclick", function () {
-  $("#fish1Id").stop();
+  $("#fish1Id").stop(); 
   $(this)
     .animate({
       height: "+=30",
@@ -35,6 +24,7 @@ $("#fish1Id").on("dblclick", function () {
       height: "250",
     });
   $("#fish1Id").myfunction();
+ 
   // $("#newFish").append('<img id="fish1Id" src="images/fish1.png" alt="">');
   // $("#fish1Id").myfunction();
 });
@@ -54,39 +44,36 @@ $("#fish2Id").on("mouseover", function () {
     },
     1000
   );
-  $("#fish2Id").myfunction();
+$("#fish2Id").myfunction();
 });
 
+
+
+
+
 // moving fish
-(function ($) {
+ (function ($) {
   $.fn.myfunction = function () {
     let h = $(document).height() - 150;
     let w = $(document).width() - 150;
     let forHeight = Math.floor(Math.random() * h + 1);
-    150;
     let forWidth = Math.floor(Math.random() * w + 1);
     $(this).animate(
       {
         left: forWidth,
         top: forHeight,
       },
-      6000
+      6000,
+    function(){
+      $("#fish1Id").myfunction();
+      $("#fish2Id").myfunction();
+    }
     );
-  };
-})(jQuery);
-// loops remain
-
-//
+};
+ })
+(jQuery);
 
 
-
-
-
-// Created a "removing"  function for this one
-// $("#bubble1Id").click(function(){
-//   $("#bubble1Id").stop();
-//   $("#bubble1Id").fadeOut();
-// });
 
 // FUnction to remove bubbles
 
@@ -94,28 +81,51 @@ $("#fish2Id").on("mouseover", function () {
   $.fn.removing = function () {
     $(this).on("click", function () {
       $(this).stop();
-      $(this).fadeOut();
+      $(this).toggle();
+      
     });
   };
 })(jQuery);
 
-// bubble 1
-a = 2;
-do {
-  $("#bubble1Id").css({ top: 800, left: 100 });
+// // bubble 1
+// a = 2;
+// do {
+//   $("#bubble1Id").css({ top: 800, left: 100 });
 
-  $("#bubble1Id").animate({ left: 100, top: -100 }, 6000);
+//   $("#bubble1Id").animate({ left: 100, top: -100 }, 6000);
 
-  $("#bubble1Id").removing();
+//   $("#bubble1Id").removing();
 
-  //     // bubble 2
-  $("#bubble2Id").css({ top: 800, left: 300 });
-  $("#bubble2Id").animate({ left: 300, top: -100 }, 6000);
-  $("#bubble2Id").removing();
+//   //     // bubble 2
+//   $("#bubble2Id").css({ top: 800, left: 300 });
+//   $("#bubble2Id").animate({ left: 300, top: -100 }, 6000);
+//   $("#bubble2Id").removing();
 
-  // bubble3
-  $("#bubble3Id").css({ top: 800, left: 600 });
-  $("#bubble3Id").animate({ left: 600, top: -100 }, 6000);
-  $("#bubble3Id").removing();
-  a++;
-} while (a < 10);
+//   // bubble3
+//   $("#bubble3Id").css({ top: 800, left: 600 });
+//   $("#bubble3Id").animate({ left: 600, top: -100 }, 6000);
+//   $("#bubble3Id").removing();
+//   a++;
+// } while (a < 10);
+
+
+
+function bubbleAnimation(bubble,num){
+  let a = num;
+  let w = $(window).width();
+  let forWidth = Math.floor(Math.random() * w + 1) + 100;
+  let forTime = Math.floor(Math.random()*(10000-6000)) + 6000;
+  bubble
+  .css({top:$(window).height()+100, left:forWidth})
+  .animate({top:-100,left:forWidth},forTime,function(){
+    bubbleAnimation(bubble);
+  });
+}
+$("#bubble3Id").removing();
+$("#bubble2Id").removing();
+$("#bubble1Id").removing();
+bubbleAnimation($("#bubble1Id"));
+
+bubbleAnimation($("#bubble2Id"));
+
+bubbleAnimation($("#bubble3Id"));
