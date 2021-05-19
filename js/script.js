ec8026f2b4e25a4df4a04c1a0efeec3201c48c51
himal28924 <himal28924@gmail.com>
2021-05-19 22:21:58 +0200

    Except for dblclick
    Except for double click on line 14 everything is good
    1.Fish come to place while clicked,
    2.Bubble come back again
    3.Blue fish moves

--- js/script.js
+++ js/script.js
@@ -4,29 +4,28 @@ $("html").on("click", function (e) {
   $("#fish1Id").stop();
   let x = e.pageX - 80;
   let y = e.pageY - 80;
+  $("#fish1Id").stop();
   $("#fish1Id").animate({ left: x, top: y });
   $("#fish1Id").myfunction();
 });
 
 
 
-// increasing the size of orange fish while clicking
+// // increasing the size of orange fish while clicking
 $("#fish1Id").on("dblclick", function () {
   $("#fish1Id").stop(); 
-  $(this)
+  $("#fish1Id")
     .animate({
       height: "+=30",
       width: "+=30",
     })
     .delay(1000)
     .animate({
-      width: "250",
-      height: "250",
+      width: "-=30",
+      height: "-=30",
     });
-  $("#fish1Id").myfunction();
- 
-  // $("#newFish").append('<img id="fish1Id" src="images/fish1.png" alt="">');
-  // $("#fish1Id").myfunction();
+    $("#fish1Id").myfunction();
+
 });
 
 // for blue fish while hoverning it
@@ -72,8 +71,8 @@ $("#fish2Id").myfunction();
 };
  })
 (jQuery);
-
-
+$("#fish1Id").myfunction();
+$("#fish2Id").myfunction();
 
 // FUnction to remove bubbles
 
@@ -81,32 +80,14 @@ $("#fish2Id").myfunction();
   $.fn.removing = function () {
     $(this).on("click", function () {
       $(this).stop();
-      $(this).toggle();
+      bubbleAnimation($(this));
+$(this).removing();
+
+
       
     });
   };
 })(jQuery);
-
-// // bubble 1
-// a = 2;
-// do {
-//   $("#bubble1Id").css({ top: 800, left: 100 });
-
-//   $("#bubble1Id").animate({ left: 100, top: -100 }, 6000);
-
-//   $("#bubble1Id").removing();
-
-//   //     // bubble 2
-//   $("#bubble2Id").css({ top: 800, left: 300 });
-//   $("#bubble2Id").animate({ left: 300, top: -100 }, 6000);
-//   $("#bubble2Id").removing();
-
-//   // bubble3
-//   $("#bubble3Id").css({ top: 800, left: 600 });
-//   $("#bubble3Id").animate({ left: 600, top: -100 }, 6000);
-//   $("#bubble3Id").removing();
-//   a++;
-// } while (a < 10);
 
 
 
@@ -114,12 +95,14 @@ function bubbleAnimation(bubble){
   let w = $(window).width();
   let forWidth = Math.floor(Math.random() * w + 1) + 100;
   let forTime = Math.floor(Math.random()*(10000-6000)) + 6000;
+  let pleDont = forWidth
   bubble
   .css({top:$(window).height()+100, left:forWidth})
-  .animate({top:-100,left:forWidth},forTime,function(){
+  .animate({top:-100,pleDont},forTime,function(){
     bubbleAnimation(bubble);
   });
 }
+
 $("#bubble3Id").removing();
 $("#bubble2Id").removing();
 $("#bubble1Id").removing();
