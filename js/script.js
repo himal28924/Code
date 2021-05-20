9f39144cc1b6865ccc6949b3888895e0ff629d97
himal28924 <himal28924@gmail.com>
2021-05-20 16:45:35 +0200

    Added bartosz program

--- js/script.js
+++ js/script.js
@@ -78,7 +78,24 @@ $('#houseOfOctopus').on("click",function()
 });
 
 
+// -------
+function moveDiv() {
+  var $span = $("#fish3Id");
 
+  $span.fadeOut(1000, function() {
+      var maxLeft = $(window).width() - $span.width();
+      var maxTop = $(window).height() - $span.height();
+      var leftPos = Math.floor(Math.random() * (maxLeft + 1))
+      var topPos = Math.floor(Math.random() * (maxTop + 1))
+
+      $span.css({ left: leftPos, top: topPos }).fadeIn(1000);
+  });
+};
+
+moveDiv();
+setInterval(moveDiv, 5000);
+
+// -------
 
 // moving fish
  (function ($) {
@@ -147,3 +164,4 @@ bubbleAnimation($("#bubble1Id"));
 bubbleAnimation($("#bubble2Id"));
 
 bubbleAnimation($("#bubble3Id"));
+
